3377f5321174792c768e67743a1457f2201acf8a
Daniel Cheley <danielcheley@hotmail.com>
2017-02-11 02:16:42 -0500

    Adding JQuery fade in for logos, welcome and side nav

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,10 @@
 //= require jquery
 //= require materialize
 $(document).ready( function() {
-
+  $('.brand-logo').hide();
+  $('.welcome').hide();
+  $('.side-nav').hide();
+  $('.brand-logo').fadeIn(3000);
+  $('.welcome').fadeIn(3000);
+  $('.side-nav').fadeIn(3000);
 });
