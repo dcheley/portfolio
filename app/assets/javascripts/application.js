c1c9c412ff6f455d00ea4b98804f8ff515048601
Daniel Cheley <danielcheley@hotmail.com>
2017-02-11 03:15:00 -0500

    Indenting code, changing element sizes and adjusting scrollfire

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,12 +4,19 @@ $(document).ready( function() {
   $('body').hide();
   $('body').fadeIn(3000);
   $(".play").hide();
+  $(".background").hide();
   $(window).scroll(function() {
     var y_scroll_pos = window.pageYOffset;
     var scroll_pos_test = 50;
+    var scroll_pos_test_2 = 190;
 
     if(y_scroll_pos > scroll_pos_test) {
       $(".play").fadeIn(3000);
     }
+
+    if(y_scroll_pos > scroll_pos_test_2) {
+      $(".background").fadeIn(5000);
+    }
+
   });
 });
