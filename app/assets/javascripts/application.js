6f3041714aa5f352bead87a453dcbe299d8984b4
Daniel Cheley <danielcheley@hotmail.com>
2017-02-11 03:02:48 -0500

    Reducing .page height and adding fadeIn on scroll

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,4 +3,13 @@
 $(document).ready( function() {
   $('body').hide();
   $('body').fadeIn(3000);
+  $(".play").hide();
+  $(window).scroll(function() {
+    var y_scroll_pos = window.pageYOffset;
+    var scroll_pos_test = 50;
+
+    if(y_scroll_pos > scroll_pos_test) {
+      $(".play").fadeIn(3000);
+    }
+  });
 });
