f38b990562cef1e0bcb0d7ae52bc08d675a891a7
dcheley <danielcheley@hotmail.com>
2017-03-28 16:54:26 -0400

    Setting up turbolinks and fixing materialie carousel bug by setting slider height

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,23 +1,28 @@
 //= require jquery
+//= require jquery_ujs
+//= require turbolinks
 //= require materialize.min
 $(document).ready(function() {
-  $('body').hide();
-  $('body').fadeIn(5000);
-  $(".play").hide();
-  $(".background").hide();
-  $(window).scroll(function() {
-    var y_scroll_pos = window.pageYOffset;
-    var scroll_pos_test = 100;
+  //Add turbolinks event listener for jQuery
+  $(document).on('turbolinks:load', function() {
+    $('body').hide();
+    $('body').fadeIn(5000);
+    $(".play").hide();
+    $(".background").hide();
+    $(window).scroll(function() {
+      var y_scroll_pos = window.pageYOffset;
+      var scroll_pos_test = 100;
 
-    if (y_scroll_pos > scroll_pos_test) {
-      $(".play").fadeIn(5000);
-      $(".background").fadeIn(5000);
-    }
+      if (y_scroll_pos > scroll_pos_test) {
+        $(".play").fadeIn(5000);
+        $(".background").fadeIn(5000);
+      }
+    });
+    $('.skillbar').each(function(){
+  		$(this).find('.skillbar-bar').animate({
+  			width:$(this).attr('data-percent')
+  		}, 6000);
+  	});
+    $('.carousel.carousel-slider').carousel({fullWidth: true});
   });
-  $('.skillbar').each(function(){
-		$(this).find('.skillbar-bar').animate({
-			width:$(this).attr('data-percent')
-		}, 6000);
-	});
-  $('.carousel.carousel-slider').carousel({fullWidth: true});
 });
