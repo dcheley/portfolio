e0ab79bb9d832994f733f2a270b6c717054d2565
dcheley <danielcheley@hotmail.com>
2017-05-16 17:48:00 -0400

    Entering create logic and strong params

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,5 +3,16 @@ class ContactsController < ApplicationController
   end
 
   def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      redirect_to root_url, notice: "Message sent. Thanks #{@contact.name}!"
+    else
+      render :new
+    end
+  end
+
+private
+def contact_params
+    params.require(:contact).permit(:name, :organization, :phone, :email, :message)
   end
 end
