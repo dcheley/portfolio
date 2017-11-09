1ae9578f142403bcf86fa091027a1025d5a9fc82
dcheley <danielcheley@hotmail.com>
2017-11-09 11:24:12 -0500

    Removing nav & arrows, rebranding and positioning elements

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,8 +8,8 @@ $(document).ready(function() {
   $(document).on('turbolinks:load', function() {
     $('body').hide();
     $('body').fadeIn(5000);
+    $('#neon-glow').hide();
     $('#neon-glow-2').hide();
-    $('#neon-glow-3').hide();
     $(".play").hide();
     $(".background").hide();
     $('.parallax').parallax();
@@ -18,20 +18,16 @@ $(document).ready(function() {
       var y_scroll_pos = window.pageYOffset;
       var scroll_pos_test_1 = 100;
       var scroll_pos_test_2 = 200;
-      var scroll_pos_test_3 = 300;
 
       if (y_scroll_pos > scroll_pos_test_1) {
+        $('#neon-glow').fadeIn(5000);
         $('#neon-glow').addClass('neon-glow');
       }
       if (y_scroll_pos > scroll_pos_test_2) {
-        $('#neon-glow-2').fadeIn(5000);
-        $('#neon-glow-2').addClass('neon-glow');
-      }
-      if (y_scroll_pos > scroll_pos_test_3) {
         $(".play").fadeIn(5000);
         $(".background").fadeIn(5000);
-        $('#neon-glow-3').fadeIn(5000);
-        $('#neon-glow-3').addClass('neon-glow');
+        $('#neon-glow-2').fadeIn(5000);
+        $('#neon-glow-2').addClass('neon-glow');
       }
     });
     $('.skillbar').each(function(){
