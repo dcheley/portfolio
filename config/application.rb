ac469de256649a40829dfd30bdfc60f4af858dc1
dcheley <danielcheley@hotmail.com>
2017-11-14 16:46:27 -0500

    Siwtching config settings to attempt to display parallax image

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,5 @@ module Portfolio
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.server_static_assets = true
   end
 end
