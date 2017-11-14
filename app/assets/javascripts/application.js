24405cbba05d15ee941af2d403fa94b88596b103
dcheley <danielcheley@hotmail.com>
2017-11-14 10:44:43 -0500

    Adding, removing & renaming images, changing up jquery animations & moving html elements around. Also slight styling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,29 +7,23 @@ $(document).ready(function() {
   //Add turbolinks event listener for jQuery
   $(document).on('turbolinks:load', function() {
     $('body').hide();
+    $('#intro-arrow').hide();
     $('body').fadeIn(5000);
-    $('#neon-glow').hide();
-    $('#neon-glow-2').hide();
-    $(".play").hide();
-    $(".background").hide();
     $('.parallax').parallax();
-    $(".button-collapse").sideNav();
     $(window).scroll(function() {
       var y_scroll_pos = window.pageYOffset;
       var scroll_pos_test = 125;
 
       if (y_scroll_pos > scroll_pos_test) {
-        $(".play").fadeIn(5000);
-        $(".background").fadeIn(5000);
-        $('#neon-glow').fadeIn(5000);
-        $('#neon-glow').addClass('neon-glow');
+        $('#intro-arrow').fadeIn(5000)
+        $('.skillbar').each(function(){
+          $(this).find('.skillbar-bar').animate({
+            width:$(this).attr('data-percent')
+          }, 6000);
+        });
       }
     });
-    $('.skillbar').each(function(){
-  		$(this).find('.skillbar-bar').animate({
-  			width:$(this).attr('data-percent')
-  		}, 6000);
-  	});
+
     $('.carousel.carousel-slider').carousel({fullWidth: true});
   });
 });
