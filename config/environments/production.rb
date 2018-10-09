89b3ca45c0b51a2ad4c9b7c05407129ea1d6733f
dcheley <danielcheley@hotmail.com>
2018-10-09 18:34:39 -0400

    config.read_encrypted_secrets = true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,11 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+  # Attempt to read encrypted secrets from `config/secrets.yml.enc`.
+  # Requires an encryption key in `ENV["RAILS_MASTER_KEY"]` or
+  # `config/secrets.yml.key`.
+  config.read_encrypted_secrets = true
+  
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
